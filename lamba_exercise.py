13426c610315da42dea590296a7d5193ec83f90e
Andrew R <uplynksolutions@UplynkSonsM1max.local>
2022-12-28 11:42:58 -0500

    lambda implementation

---
+++ lamba_exercise.py
@@ -0,0 +1,49 @@
+import re
+
+prog_lang=[('Python',3.8),('Java',13),('JavaScript',2019),('Scala', 2.13)]
+
+###Sort the list by each language's version in ascending order###
+prog_lang.sort(key=lambda x:x[1] )
+print(prog_lang)
+
+###Sort the list by the length of the name of each language in descending order###
+len_name =prog_lang
+new_lst=sorted(len_name,key=lambda x:x[1],reverse =True)
+print (new_lst)
+
+###Filter the list so that it only contains languages with 'a' in it.###
+#
+fil=list(filter(lambda x:re.findall(r'[a]+',x[0]),prog_lang))
+print(f"The languages with letter a are : {fil}")
+
+###Filter the list so that it only contains languages whose version is in integer form.###
+
+int_form=list(filter(lambda x: x if(type(x[1])==int) else 0 , prog_lang))
+print(f" The languages with int values: {int_form}")
+
+###Transform the list so that it contains the tuples in the form,
+# ("language in all lower case", length of the language string)###
+ 
+
+###test regular functions###
+# def check(lst):
+#     example=[]
+#     for i in (prog_lang): 
+#         a,b= (str(i[0].lower()),len(i[0]))
+#         example.append(((a,b)))
+        
+#     print (example)
+    
+# check(prog_lang)
+lst=[]
+def newlst(lst_val):
+    for i in lst_val:
+        lst.append((i[0].lower(),len(i[0])))
+    print(lst)
+
+convertnew=filter(newlst(prog_lang),prog_lang)
+
+
+###Generate a tuple in the form,("All languages separated by commas","All versions separated by commas")##
+# ##
+#('Python,Java,JavaScript,Scala', '3.8,13,2019,2.13')
